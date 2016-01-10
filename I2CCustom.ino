73a79584913ccf27f788f2103eed717933f06472
Javier Pollak <javi.po.123@gmail.com>
2016-01-10 10:10:16 -0500

    Half-way to adding print line by line and scrolling when no space left

--- I2CCustom.ino
+++ I2CCustom.ino
@@ -2,8 +2,13 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 /**********************************************************/
-char array1[] = "Hola Javier               ";  //the string to print on the LCD
-char array2[] = "Probando esribir aqui!@#$%";  //the string to print on the LCD
+char array1[] = "something......";  //the string to print on the LCD
+char array2[] = "words are cool.";  //the string to print on the LCD
+char array3[] = "I am very nice.";
+char array4[] = "you are awesome";
+
+//char paragraph[][] = {array1, array2, array3, array4};
+
 int tim = 25;  //the value of delay time
 			   // initialize the library with the numbers of the interface pins
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
@@ -19,7 +24,7 @@ void loop()
 {
 	tim = 25;
 	lcd.setCursor(0, 0);  // set the cursor to column 15, line 0
-	for (int positionCounter1 = 0; positionCounter1 < 26; positionCounter1++)
+	for (int positionCounter1 = 0; positionCounter1 < 15; positionCounter1++)
 	{
 		//lcd.scrollDisplayLeft();  //Scrolls the contents of the display one space to the left.
 		lcd.print(array1[positionCounter1]);  // Print a message to the LCD.
@@ -27,18 +32,13 @@ void loop()
 	}
 	// lcd.clear();  //Clears the LCD screen and positions the cursor in the upper-left corner.
 	lcd.setCursor(0, 1);  // set the cursor to column 15, line 1
-	for (int positionCounter = 0; positionCounter < 26; positionCounter++)
+	for (int positionCounter = 0; positionCounter < 15; positionCounter++)
 	{
-		if (positionCounter >= 16)
-		{
-			tim = 250;
-			lcd.scrollDisplayLeft();
-		}
-
+		
 		//lcd.scrollDisplayLeft();  //Scrolls the contents of the display one space to the left.
 		lcd.print(array2[positionCounter]);  // Print a message to the LCD.
 		delay(tim);  //wait for 250 microseconds
 	}
-	lcd.clear();  //Clears the LCD screen and positions the cursor in the upper-left corner.
+	//lcd.clear();  //Clears the LCD screen and positions the cursor in the upper-left corner.
 }
 /************************************************************/
